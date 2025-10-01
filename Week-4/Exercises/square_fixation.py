d4239f3c136837aac755b320a9e05184b5638669
Anna <anna.krasovskaya@ens-rennes.fr>
2025-10-01 18:30:47 +0200

    week 4 exo

---
+++ Week-4/Exercises/square_fixation.py
@@ -0,0 +1,17 @@
+from expyriment import design, control, stimuli
+
+exp = design.Experiment(name="Square")
+
+control.set_develop_mode()
+control.initialize(exp)
+
+fixation = stimuli.FixCross()
+square = stimuli.Rectangle(size=(100, 100), line_width=5)
+
+control.start(subject_id=1)
+
+fixation.plot(square)
+square.present(clear=False, update=True)
+exp.keyboard.wait()
+
+control.end()
